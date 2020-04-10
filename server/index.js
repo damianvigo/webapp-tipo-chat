b7553cae78e4c25b15831a7aa53bb4b4adf75fb3
Damian <damiandanielvigo@gmail.com>
2020-04-09 22:39:30 -0300

    conexion-socket

--- server/index.js
+++ server/index.js
@@ -3,9 +3,17 @@ let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 
+app.use(express.static('client'));
+
+
 app.get('/ruta-uno', function(req, res) {
   res.status(200).send('Desde la ruta 1');
 });
+
+io.on('connection', function(socket) {
+  console.log('El cliente: con IP ' + socket.handshake.address + 'se ha conectado');
+
+})
 
 server.listen(6677, function() {
   console.log('Servidor funcionando en http://localhost:6677');
